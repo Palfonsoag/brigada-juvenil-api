37abefa92b19267a940dffb19d7bfc418108e480
Pedro Alfonso <p.alfonsoag@gmail.com>
2020-05-29 00:31:34 -0400

    fix on allergies module

--- src/allergies/allergies.module.ts
+++ src/allergies/allergies.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AllergiesService } from './allergies.service';
-import { AllergiesController } from './allergies.controller';
+import { Module } from "@nestjs/common";
+import { AllergiesService } from "./allergies.service";
+import { AllergiesController } from "./allergies.controller";
+import { AllergyRepository } from "./allergy.repository";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AllergyRepository])],
   providers: [AllergiesService],
-  controllers: [AllergiesController]
+  controllers: [AllergiesController],
 })
 export class AllergiesModule {}
